ad2dd3ea4a0a2029ad663446c0fbad1a279eb88b
Qanafya <91042967+Qanafya@users.noreply.github.com>
2022-04-26 23:11:16 +0600

    Update settings.py

--- Qaz/settings.py
+++ Qaz/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-flj)5ji&)^9vk14a5f2b92i2@f1vss78s-yfzx6re*$%rlnyjr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'qanafya.herokuapp.com']
 
 
 # Application definition
